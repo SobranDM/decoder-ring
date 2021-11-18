fd023b17d04e6a631ce440576924174aa880b0e6
SobranDM <sobran@gmail.com>
2021-11-18 14:54:55 -0800

    caesar function done

--- src/caesar.js
+++ src/caesar.js
@@ -7,7 +7,27 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    if (shift === 0 || shift < -25 || shift > 25) return false;
+    let shiftMod;
+    encode ? shiftMod = shift : shiftMod = 0 - shift;
+    const message = input.toLowerCase();
+    let charArray = [];
+    for (let i = 0; i < message.length; i++) {
+      const currChar = message.charCodeAt(i);
+      if (currChar > 122 || currChar < 97) {
+        charArray.push(currChar);
+      } else {
+        let shiftWrap = currChar + shiftMod;
+        if (shiftWrap > 122) {
+          charArray.push(shiftWrap - 26);
+        } else if (shiftWrap < 97) {
+          charArray.push(shiftWrap + 26);
+        } else {
+          charArray.push(shiftWrap);
+        }
+      }
+    }
+    return String.fromCharCode(...charArray);
   }
 
   return {
